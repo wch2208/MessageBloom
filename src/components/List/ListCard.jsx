7248943a345b2ededb78a1873c07bf3a3106f4cf
Hyunji <hjcho2303@naver.com>
2024-05-01 21:46:54 +0900

    Feat: 가장 많이 받은 이모지들 컴포넌트로 분리

--- src/components/List/ListCard.jsx
+++ src/components/List/ListCard.jsx
@@ -1,11 +1,12 @@
 import '../../styles/List/ListCard.scss';
+import './ShowTheMostEmojis';
+import ShowTheMostEmojis from './ShowTheMostEmojis';
 
 export default function ListCard() {
   return (
     <div className='list-card'>
       <div className='list-card__writers'>
         <p className='list-card__writers__to'>To.Sowon</p>
-
         <div className='list-card__writers__profiles'>
           <div className='list-card__writers__profiles-img'>
             <img
@@ -38,20 +39,7 @@ export default function ListCard() {
 
       <div className='list-card__emoji-container'>
         <div className='list-card__divider'></div>
-        <div className='list-card__emoji-wrap'>
-          <div className='list-card__emoji'>
-            <span>🥹</span>
-            <span>20</span>
-          </div>
-          <div className='list-card__emoji'>
-            <span>🥹</span>
-            <span>20</span>
-          </div>
-          <div className='list-card__emoji'>
-            <span>🥹</span>
-            <span>20</span>
-          </div>
-        </div>
+        <ShowTheMostEmojis />
       </div>
     </div>
   );
