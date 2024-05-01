8a5ae3fac7a6d9dd37c6a62b012b679c8bce9ce1
WooGi1020 <jungwk1020@gmail.com>
2024-05-01 18:35:28 +0900

    feat #12 : Modal handling을 위한 State 추가 및 Modal 렌더링 로직 구현

--- src/pages/PostedPage.jsx
+++ src/pages/PostedPage.jsx
@@ -3,13 +3,17 @@ import PostCard from '../components/posted-page/PostCard';
 import '../styles/posted-page/PostedPage.scss';
 import imgPofile01 from '../assets/image/profile/img_profile_01.svg';
 import plusicon from '../assets/icon/ic_plus.svg';
+import { useState } from 'react';
+import Modal from '../components/posted-page/Modal';
+
 const mockData = [
   {
     id: 1,
     profile: imgPofile01,
     name: '김동훈',
     role: '동료',
-    content: '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!',
+    content:
+      '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!',
     date: new Date().toLocaleDateString(),
   },
   {
@@ -39,6 +43,18 @@ const mockData = [
 ];
 
 function PostedPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalData, setModalData] = useState([]);
+
+  const setModalDataByData = (modalId) => {
+    const [modalItem] = mockData.filter((data) => data.id == modalId);
+    setModalData(modalItem);
+  };
+
+  const handleModalOpen = (value) => {
+    setIsModalOpen(value);
+  };
+
   return (
     <div className='posted-page-container'>
       <div className='add-post-card'>
@@ -47,8 +63,17 @@ function PostedPage() {
         </div>
       </div>
       {mockData.map((data) => {
-        return <PostCard key={data.id} data={data} />;
+        return (
+          <div key={data.id}>
+            <PostCard
+              setModalDataByData={setModalDataByData}
+              handleModalOpen={handleModalOpen}
+              data={data}
+            />
+          </div>
+        );
       })}
+      {isModalOpen && <Modal handleModalOpen={handleModalOpen} modalData={modalData} />}
     </div>
   );
 }
