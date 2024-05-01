6225a3331c61b9c3dd424d4288e9a1f528c95c0a
WooGi1020 <jungwk1020@gmail.com>
2024-05-01 18:32:06 +0900

    feat : Modal.jsx 생성 및 html 태그 로직 작성

---
+++ src/components/posted-page/Modal.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import getClassByRole from '../../utils/getClassByRole';
+import '../../styles/posted-page/Modal.scss';
+
+function Modal({ modalData, handleModalOpen }) {
+  return (
+    <div className='modal-wrapper'>
+      <div className='modal-container'>
+        <div className='modal-info'>
+          <img
+            className='modal-info__img'
+            src={modalData.profile}
+            alt='받은 포스트 카드 작성자 프로필 이미지'
+          />
+          <div className='modal-info-detail-container'>
+            <p className='modal-info__name'>From. {modalData.name}</p>
+            <span className={`modal-info__role ${getClassByRole(modalData.role)}`}>
+              {modalData.role}
+            </span>
+          </div>
+          <span className='modal__date'>{modalData.date}</span>
+        </div>
+        <div className='modal__underline'></div>
+        <div className='modal__content-container'>
+          <p className='modal__content'>{modalData.content}</p>
+        </div>
+        <button
+          onClick={() => {
+            handleModalOpen(false);
+          }}
+          id='modal-confirm__btn'>
+          확인
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
