3b716ce9d062d4e2bcde19f06cc17dd3405977f8
Hyunji <hjcho2303@naver.com>
2024-05-02 09:38:25 +0900

    Feat: mock-data 연결 후 배경색상 변경

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,26 +1,42 @@
+import { useState } from 'react';
 import '../styles/List/List.scss';
 import ListCard from '../components/List/ListCard';
+import list from '../mock-data.json';
+
+const mock = [
+  {
+    id: 1,
+    name: '조현지',
+    backgroundColor: 'orange',
+    backgroundImageURL: '',
+    createAt: '2024-05-01',
+    messageCount: '1',
+    recentMessage: '',
+    reactionCount: 0,
+    topReactions: '',
+  },
+];
 
 export default function List() {
+  const [data, setData] = useState(list.list);
+
   return (
     <div className='list'>
       <div className='list__best'>
         <p className='list__best-title'>인기 롤링 페이퍼 🔥</p>
         <div className='list__best-cards'>
-          <ListCard />
-          <ListCard />
-          <ListCard />
-          <ListCard />
+          {data.map((card) => (
+            <ListCard key={card.id} {...card} />
+          ))}
         </div>
       </div>
 
       <div className='list__newest'>
         <p className='list__newest-title'>최근에 만든 롤링 페이퍼 ⭐️</p>
         <div className='list__newest-cards'>
-          <ListCard />
-          <ListCard />
-          <ListCard />
-          <ListCard />
+          {data.map((card) => (
+            <ListCard key={card.id} {...card} />
+          ))}
         </div>
       </div>
 
