b2902192a1f1b6a1ada8149f2fef459943f4d6fe
WooGi1020 <jungwk1020@gmail.com>
2024-05-02 11:18:22 +0900

    chore #12 : assets 받아오는 네이밍 소문자로 변경

--- src/pages/PostedPage.jsx
+++ src/pages/PostedPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PostCard from '../components/posted-page/PostCard';
 import '../styles/posted-page/PostedPage.scss';
-import imgPofile01 from '../assets/image/profile/img_profile_01.svg';
+import imgpofile01 from '../assets/image/profile/img_profile_01.svg';
 import plusicon from '../assets/icon/ic_plus.svg';
 import { useState } from 'react';
 import Modal from '../components/posted-page/Modal';
@@ -11,7 +11,7 @@ const mockData = [
     id: 1,
     recipientId: 1,
     sender: '김동훈',
-    profileImageURL: imgPofile01,
+    profileImageURL: imgpofile01,
     relationship: '친구',
     content:
       '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!',
@@ -22,7 +22,7 @@ const mockData = [
     id: 2,
     recipientId: 1,
     sender: '김동훈',
-    profileImageURL: imgPofile01,
+    profileImageURL: imgpofile01,
     relationship: '동료',
     content:
       '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!',
@@ -33,7 +33,7 @@ const mockData = [
     id: 3,
     recipientId: 1,
     sender: '김동훈',
-    profileImageURL: imgPofile01,
+    profileImageURL: imgpofile01,
     relationship: '지인',
     content:
       '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!',
@@ -44,7 +44,7 @@ const mockData = [
     id: 4,
     recipientId: 1,
     sender: '김동훈',
-    profileImageURL: imgPofile01,
+    profileImageURL: imgpofile01,
     relationship: '가족',
     content:
       '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요.',
@@ -55,7 +55,7 @@ const mockData = [
     id: 5,
     recipientId: 1,
     sender: '김동훈',
-    profileImageURL: imgPofile01,
+    profileImageURL: imgpofile01,
     relationship: '동료',
     content:
       '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 조심하세요!',
