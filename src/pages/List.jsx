8e767c2a6f5a5d4fa0775a856998c3b5ed40f891
Hyunji <hjcho2303@naver.com>
2024-05-03 13:43:37 +0900

    Refactor:  변경사항 저장

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,16 +1,13 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import '../styles/List/List.scss';
 import ListCard from '../components/List/ListCard';
-import list from '../mock-data.json';
+import list from '../recipients-mock.json';
 import ArrowButton from '../components/List/ArrowButton';
 import ic_arrow_left from './../assets/icon/ic_arrow_left.svg';
 import ic_arrow_right from './../assets/icon/ic_arrow_right.svg';
 
 export default function List() {
   const [data, setData] = useState(list.list);
-  const scrollRef = useRef(null);
-  const bestCardsRef = useRef(null);
-  const latestCardsRef = useRef(null);
 
   const getBestCards = () => {
     const sortedCards = [...data];
@@ -21,33 +18,25 @@ export default function List() {
   const getLatestCards = () => {
     const sortedCards = [...data];
     sortedCards.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-    return sortedCards;
+    return sortedCards.slice(0, 4);
   };
 
   const bestCards = getBestCards();
   const latestCards = getLatestCards();
 
-  const handleClickScroll = (direction) => {
-    if (direction === 'left') {
-      scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    } else if (direction === 'right') {
-      scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'end' });
-    }
-  };
-
   return (
     <div className='list'>
       <div className='list__best'>
         <p className='list__best-title'>인기 롤링 페이퍼 🔥</p>
-        <ArrowButton btnType={'left'} onClick={handleClickScroll()}>
+        <ArrowButton btnType={'left'}>
           {<img src={ic_arrow_left} alt='왼쪽으로 클릭'></img>}
         </ArrowButton>
         <div className='list__best-cards'>
           {bestCards.map((card) => (
-            <ListCard ref={scrollRef.current} key={card.id} {...card} />
+            <ListCard key={card.id} {...card} />
           ))}
         </div>
-        <ArrowButton btnType={'right'} onClick={handleClickScroll}>
+        <ArrowButton btnType={'right'}>
           {<img src={ic_arrow_right} alt='오른쪽으로 클릭'></img>}
         </ArrowButton>
       </div>
