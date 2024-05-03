15732a2794e3b232e81ceff1d5a69dd3afa657af
chanhee won <wch2208@gmail.com>
2024-05-03 22:33:01 +0900

    feat #29: post페이지 api 요청을 위한 데이터 세팅
    - 컬러, 이미지 토글 시 이전 값 초기화
      - 컬러: beige, 이미지: null
    - formData를 만드는 컴포넌트에서 setState를 ColorPicker, ImagePicker로 전달
    - formData의 초기값 FormConfig에 상수로 저장
    - 이미지주소를 wallpaper01 형식으로 사용하기 위해 FormConfig에 추가

--- src/pages/PostToPage.jsx
+++ src/pages/PostToPage.jsx
@@ -1,7 +1,29 @@
 import '../styles/create-message-form/PostToPage.scss';
 import ToggleOption from '../components/create-message-form/ToggleOption';
+import { useState } from 'react';
+import { DEFAULT_RECIPIENT } from '../components/create-message-form/FormConfig';
 
 export default function PostToPage() {
+  const [recipient, setRecipient] = useState(DEFAULT_RECIPIENT);
+  const [recipientName, setRecipientName] = useState('');
+
+  const updateRecipientName = () => {
+    setRecipient({ ...recipient, name: recipientName });
+    document.getElementById('toInput').value = '';
+  };
+
+  const handleCreateClick = () => {
+    updateRecipientName();
+    const formData = new FormData();
+    formData.append('data', JSON.stringify(recipient));
+    // TODO: api post 요청
+  };
+
+  const handleChange = (e) => {
+    setRecipientName(e.target.value);
+    //TODO: 유효성 검사 추가
+  };
+
   return (
     <>
       <div className='background-form'>
@@ -11,15 +33,18 @@ export default function PostToPage() {
             className='background-form__input'
             id='toInput'
             placeholder='받는 사람 이름을 입력해 주세요'
+            onChange={handleChange}
           />
         </div>
         <div className='background-form-group'>
           <p>배경화면을 선택해주세요.</p>
           <p>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
         </div>
-        <ToggleOption />
+        <ToggleOption setRecipient={setRecipient} />
 
-        <button className='background-form__submit'>생성하기</button>
+        <button className='background-form__submit' onClick={handleCreateClick}>
+          생성하기
+        </button>
       </div>
     </>
   );
