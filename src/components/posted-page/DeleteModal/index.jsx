d623e68241598ef1cff4d5f219ba62e081784209
WooGi1020 <jungwk1020@gmail.com>
2024-05-04 18:23:32 +0900

    feat #33 : DeleteModal.jsx 생성 및 모달, 메세지 삭제 로직 구현

---
+++ src/components/posted-page/DeleteModal/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './DeleteModal.scss';
+import { deleteMessage } from '../../../apis/api';
+
+function DeleteModal({ handleDeleteMessage, deleteDataId, handleDeleteModalOpen }) {
+  const handleDeleteData = async (id) => {
+    await deleteMessage(id);
+    handleDeleteCard(id);
+  };
+
+  const handleDeleteCard = (id) => {
+    handleDeleteMessage(id);
+  };
+
+  const handleCheckClick = () => {
+    handleDeleteData(deleteDataId);
+    handleDeleteModalOpen(false);
+  };
+
+  const handleCancelClick = () => {
+    handleDeleteModalOpen(false);
+  };
+
+  return (
+    <div className='delete-modal-wrapper'>
+      <div className='delete-modal-container'>
+        <p className='delete-modal-container__message'>정말 삭제하시겠어요?</p>
+        <div className='delete-modal-container__btns'>
+          <button id='delete-modal-container__btns-check-btn' onClick={handleCheckClick}>
+            확인
+          </button>
+          <button id='delete-modal-container__btns-cancel-btn' onClick={handleCancelClick}>
+            취소
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DeleteModal;
