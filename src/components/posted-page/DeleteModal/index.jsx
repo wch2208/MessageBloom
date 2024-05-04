d085726183b9216657baf96ee52549c36a91dccf
WooGi1020 <jungwk1020@gmail.com>
2024-05-04 18:35:08 +0900

    feat #33 : 모달창 상위 요소 클릭시 모달 OFF 및 하위 요소 버블링 방지 함수 작성

--- src/components/posted-page/DeleteModal/index.jsx
+++ src/components/posted-page/DeleteModal/index.jsx
@@ -22,8 +22,12 @@ function DeleteModal({ handleDeleteMessage, deleteDataId, handleDeleteModalOpen 
   };
 
   return (
-    <div className='delete-modal-wrapper'>
-      <div className='delete-modal-container'>
+    <div className='delete-modal-wrapper' onClick={handleCancelClick}>
+      <div
+        className='delete-modal-container'
+        onClick={(e) => {
+          e.stopPropagation();
+        }}>
         <p className='delete-modal-container__message'>정말 삭제하시겠어요?</p>
         <div className='delete-modal-container__btns'>
           <button id='delete-modal-container__btns-check-btn' onClick={handleCheckClick}>
