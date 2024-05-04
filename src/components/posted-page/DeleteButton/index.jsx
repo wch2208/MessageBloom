7ef445b68c4f5ca14c508503280998d966a7ad98
WooGi1020 <jungwk1020@gmail.com>
2024-05-04 10:56:46 +0900

    feat #33 : API 사용하지 않은 상태로 삭제 버튼 동작 테스트

--- src/components/posted-page/DeleteButton/index.jsx
+++ src/components/posted-page/DeleteButton/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './DeleteButton.scss';
 import deletedicon from '../../../assets/icon/ic_deleted.svg';
+import { deleteMessage } from '../../../apis/api';
 
-function DeleteButton(props) {
+function DeleteButton({ id, handleDeleteMessage }) {
+  const handleDelete = async (id) => {
+    // await deleteMessage(id);
+    handleDeleteMessage(id);
+  };
+
+  const handleDeleteClick = () => {
+    handleDelete(id);
+  };
+
   return (
-    <button id='card__delete-btn'>
+    <button id='card__delete-btn' onClick={handleDeleteClick}>
       <img src={deletedicon} alt='포스트 카드 삭제 버튼' />
     </button>
   );
