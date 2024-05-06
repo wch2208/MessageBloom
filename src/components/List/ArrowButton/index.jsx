f6b9c2d73e3e98dec6388478539957a173a150f0
Hyunji <hjcho2303@naver.com>
2024-05-06 13:31:45 +0900

    Style: 화살표 버튼 수정

--- src/components/List/ArrowButton/index.jsx
+++ src/components/List/ArrowButton/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './ArrowButton.scss';
 
-export default function ArrowButton({ btnType, onClick, children }) {
+import ic_arrow_left from '../../../assets/icon/ic_arrow_left.svg';
+import ic_arrow_right from '../../../assets/icon/ic_arrow_right.svg';
+
+export function ArrowButtonLeft() {
   return (
-    <button className={`arrow-button ${btnType}`} onClick={onClick}>
-      {children}
+    <button className='arrow-button left'>
+      <img src={ic_arrow_left} alt='왼쪽으로 클릭'></img>
+    </button>
+  );
+}
+
+export function ArrowButtonRight() {
+  return (
+    <button className='arrow-button right'>
+      <img src={ic_arrow_right} alt='왼쪽으로 클릭'></img>
     </button>
   );
 }
