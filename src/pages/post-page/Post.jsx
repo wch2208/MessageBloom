580bf5435a50a66cb3df5d66345856ab3c69496c
chanhee won <wch2208@gmail.com>
2024-05-06 23:19:55 +0900

    feat #45: Post 페이지에 유효성검사 추가

--- src/pages/post-page/Post.jsx
+++ src/pages/post-page/Post.jsx
@@ -1,15 +1,24 @@
 import './Post.scss';
 import ToggleOption from '../../components/post-page/ToggleOption';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { DEFAULT_RECIPIENT } from '../../components/post-page/postPageConstants';
 import { postRecipient } from '../../apis/api';
 import { useNavigate } from 'react-router-dom';
 import LinkButton from '../../components/commons/LinkButton';
 import Header from '../../components/header/Header';
+import { useForm } from 'react-hook-form';
 
 export default function Post() {
   const [recipient, setRecipient] = useState(DEFAULT_RECIPIENT);
   const navigate = useNavigate();
+  const {
+    register,
+    formState: { errors },
+    watch,
+  } = useForm({
+    mode: 'all',
+  });
+  const toInput = watch('toInput');
 
   const handleCreateClick = async () => {
     const formData = new FormData();
@@ -18,33 +27,38 @@ export default function Post() {
     navigate(`/post/${id}`);
   };
 
-  const handleChange = (e) => {
-    setRecipient({ ...recipient, name: e.target.value });
-    //TODO: 유효성 검사 추가
-  };
+  useEffect(() => {
+    setRecipient({ ...recipient, name: toInput });
+  }, [toInput]);
 
   return (
     <>
       <Header />
-      <div className='background-form'>
+      <form className='background-form'>
         <div className='background-form__header'>
           <label className='background-form__title'>To.</label>
           <input
-            className='background-form__input'
+            className={`background-form__input ${errors.toInput && 'form--error'}`}
             id='toInput'
             placeholder='받는 사람 이름을 입력해 주세요'
-            onChange={handleChange}
             autoFocus
             autoComplete='off'
+            {...register('toInput', { required: 'Name is required' })}
           />
         </div>
+        {errors.toInput && <p className='form--error'>{errors.toInput.message}</p>}
         <div className='background-form-group'>
           <p>배경화면을 선택해주세요.</p>
           <p>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
         </div>
         <ToggleOption setRecipient={setRecipient} />
-        <LinkButton buttonText={'생성하기'} onClick={handleCreateClick} fullWidth />
-      </div>
+        <LinkButton
+          buttonText={'생성하기'}
+          onClick={handleCreateClick}
+          fullWidth
+          disabled={!!errors.toInput}
+        />
+      </form>
     </>
   );
 }
