33280105b367fb5c16b6ac09f7ef910216263f55
Hyunji <hjcho2303@naver.com>
2024-05-06 20:28:29 +0900

    Feat: 리스트 페이지 생성

--- src/pages/list-page/index.jsx
+++ src/pages/list-page/index.jsx
@@ -1,6 +1,6 @@
 import './List.scss';
 import ListCardContainer from '../../components/List/ListCardContainer/index';
-import Header from '../components/header/Header';
+import Header from '../../components/header/Header';
 
 export default function List() {
   return (
@@ -9,14 +9,13 @@ export default function List() {
       <div className='list'>
         <div className='list__best'>
           <p className='list__best-title'>인기 롤링 페이퍼 🔥</p>
-          <ListCardContainer sort='like' />
+          <ListCardContainer sortLike='like' />
         </div>
 
         <div className='list__newest'>
           <p className='list__newest-title'>최근에 만든 롤링 페이퍼 ⭐️</p>
-          <ListCardContainer sort='like' />
+          <ListCardContainer sortLike='' />
         </div>
-
         <button className='list__create-btn'>나도 만들어보기</button>
       </div>
     </>
