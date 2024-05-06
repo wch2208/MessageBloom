07f755e9a3e63adc51037cb4786accb3c26422b8
Hyunji <hjcho2303@naver.com>
2024-05-06 20:29:17 +0900

    Feat: 리스트카드 컨테이너 컴포넌트 생성

--- src/components/List/ListCardContainer/index.jsx
+++ src/components/List/ListCardContainer/index.jsx
@@ -1,27 +1,23 @@
+import './ListCardContainer.scss';
 import ListCard from '../ListCard/index';
-import ArrowButton from '../ArrowButton/index';
-import ic_arrow_left from '../../../assets/icon/ic_arrow_left.svg';
-import ic_arrow_right from '../../../assets/icon/ic_arrow_right.svg';
+import { ArrowButtonLeft, ArrowButtonRight } from '../ArrowButton/index';
 import useFetchData from '../../../hooks/useFetchData';
 import { useState, useEffect } from 'react';
 import { getRecipients } from '../../../apis/api';
-import list from '../../../recipients-mock.json';
 
 const MAX_SCREEN_WIDTH = 1200;
 const MAX_LIST_LENGTH = 8;
 const LIMIT = 4;
 const OFFSET = 0;
-const SORT_LIKE = 'like';
 
 import React from 'react';
 
-export default function ListCardContainer() {
-  const [mock, setMock] = useState(list.list);
-  const [scrollable, setScrollable] = useState(false);
+export default function ListCardContainer({ sortLike }) {
+  const [scroll, setScroll] = useState(false);
   const [limit, setLimit] = useState(LIMIT);
   const [offset, setOffset] = useState(OFFSET);
 
-  const { data, error } = useFetchData(getRecipients, [LIMIT, OFFSET, SORT_LIKE]);
+  const { data, error } = useFetchData(getRecipients, [limit, offset, sortLike]);
 
   useEffect(() => {
     updateScroll();
@@ -32,7 +28,7 @@ export default function ListCardContainer() {
 
   const updateScroll = () => {
     const smallScreen = document.documentElement.clientWidth < MAX_SCREEN_WIDTH;
-    setScrollable(smallScreen);
+    setScroll(smallScreen);
     if (smallScreen) {
       setLimit(MAX_LIST_LENGTH);
       setOffset(OFFSET);
@@ -41,19 +37,23 @@ export default function ListCardContainer() {
     }
   };
 
+  const handlePrevious = () => {
+    setOffset(offset - 1);
+  };
+
+  const handleNext = () => {
+    setOffset(offset + 1);
+  };
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <div>
-      <ArrowButton btnType={'left'}>
-        {<img src={ic_arrow_left} alt='왼쪽으로 클릭'></img>}
-      </ArrowButton>
-      <div className='list__best-cards'>
-        {mock.map((card) => (
-          <ListCard {...card} />
-        ))}
-      </div>
-      <ArrowButton btnType={'right'}>
-        {<img src={ic_arrow_right} alt='오른쪽으로 클릭'></img>}
-      </ArrowButton>
-    </div>
+    <>
+      {!scroll && offset > 0 && <ArrowButtonLeft onClick={handlePrevious} />}
+      {!scroll && offset + 4 < 8 && <ArrowButtonRight onClick={handleNext} />}
+      <div className='list__best-cards'>{data && data.map((card) => <ListCard {...card} />)}</div>
+    </>
   );
 }
