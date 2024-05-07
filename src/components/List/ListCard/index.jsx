34ecc9ae88d98068126179d3bf1c72c5b5c1fb13
Hyunji <hjcho2303@naver.com>
2024-05-07 18:11:39 +0900

    Feat: 배경이미지 연결

--- src/components/List/ListCard/index.jsx
+++ src/components/List/ListCard/index.jsx
@@ -4,14 +4,12 @@ import WritersProfiles from '../WritersProfile';
 import { useState, useEffect } from 'react';
 
 export default function ListCard({ name, backgroundColor, backgroundImageURL, messageCount }) {
-  const [background, setBackground] = useState('');
-
-  useEffect(() => {
-    backgroundColor.length > 0 ? setBackground(backgroundColor) : setBackground(backgroundImageURL);
-  }, [backgroundColor, backgroundImageURL]);
+  const backgroundImageStyle = {
+    backgroundImage: `url(${backgroundImageURL})`,
+  };
 
   return (
-    <div className={`list-card ${background ? `--${background}` : ''}`}>
+    <div className={`list-card ${`--${backgroundColor}`}`} style={backgroundImageStyle}>
       <div className='list-card__writers'>
         <p className='list-card__writers__to'>To.{name}</p>
         <WritersProfiles />
