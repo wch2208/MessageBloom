0dc305de56f88f8ed6798510ab0dd6557497a86f
WooGi1020 <jungwk1020@gmail.com>
2024-05-08 13:42:11 +0900

    refactor #70 : 버튼 텍스트 변경

--- src/components/post-id/post-pw-modal/index.jsx
+++ src/components/post-id/post-pw-modal/index.jsx
@@ -55,7 +55,7 @@ function PostPwModal({ id, handleSetPwModalOpen }) {
             />
           )}
           <button id='post-pw-modal-container__check-btn' onClick={setPwToLocalStorage}>
-            생성
+            설정
           </button>
         </div>
       </div>
