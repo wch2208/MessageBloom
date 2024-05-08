3c2f609817db6a2fa2fc5462327e4a41dc7aaafd
Hyunji <hjcho2303@naver.com>
2024-05-08 11:48:44 +0900

    Style: 배경 색상 변수 지정 및 변경

--- src/components/list-page/list-card/index.jsx
+++ src/components/list-page/list-card/index.jsx
@@ -7,14 +7,16 @@ import { useNavigate } from 'react-router-dom';
 export default function ListCard({ name, backgroundColor, backgroundImageURL, messageCount, id }) {
   const nav = useNavigate();
 
-  const [backgroundStyle] = useState(
-    backgroundImageURL ? { backgroundImage: `url(${backgroundImageURL})`, color: '#fff' } : {},
-  );
+  const baseColor = backgroundImageURL ? 'white' : '';
+  const background = backgroundImageURL ? `url(${backgroundImageURL})` : {};
 
   return (
     <div
       className={`list-card ${`--${backgroundColor}`}`}
-      style={backgroundStyle}
+      style={{
+        backgroundImage: `${background}`,
+        color: `${baseColor}`,
+      }}
       onClick={() => nav(`/post/${id}`)}>
       <div className='list-card__writers'>
         <p className='list-card__writers__to'>To.{name}</p>
