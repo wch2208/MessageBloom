ab6ebca8219837f7cdf20b24842dd9a2326ec98b
WooGi1020 <jungwk1020@gmail.com>
2024-05-08 12:56:15 +0900

    feat #70 : 포스트카드 삭제 모달 로직 구현 및 링크 연결

--- src/components/post-id/post-delete-modal/index.jsx
+++ src/components/post-id/post-delete-modal/index.jsx
@@ -1,19 +1,75 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './PostDeleteModal.scss';
+import { useNavigate } from 'react-router-dom';
+import { deleteRecipient } from '../../../apis/api';
+import showpwicon from '../../../assets/icon/ic_show_pw.svg';
+import noneshowpwicon from '../../../assets/icon/ic_none_show_pw.svg';
 
-function PostDeleteModal({ handlePostDeleteModalOpen }) {
+function PostDeleteModal({ handlePostDeleteModalOpen, id }) {
+  const [isShowPw, setIsShowPw] = useState(false);
+  const [isFailConfirm, setIsFailConfirm] = useState(false);
+  const pwRef = useRef();
+  const navigate = useNavigate();
+
+  const showPwIconClick = () => {
+    setIsShowPw(true);
+  };
+  const noneShowPwIconClick = () => {
+    setIsShowPw(false);
+  };
+
+  // 포스트 카드 수신자 제거 함수
+  const deleteRecipientData = async (id) => {
+    await deleteRecipient(id);
+  };
+
+  // 포스트 카드 삭제 취소버튼 함수
   const handleCancelClick = () => {
     handlePostDeleteModalOpen(false);
   };
 
+  // 포스트 카드 삭제 확인버튼 함수
   const handleCheckClick = () => {
-    handlePostDeleteModalOpen(false);
+    const { value } = pwRef.current;
+    if (!value) {
+      console.log('hi');
+      setIsFailConfirm(true);
+      setTimeout(() => {
+        setIsFailConfirm(false);
+      }, 1000);
+    } else if (value !== localStorage.getItem(`${id}`)) {
+      console.log('hi');
+      setIsFailConfirm(true);
+      setTimeout(() => {
+        setIsFailConfirm(false);
+      }, 1000);
+    } else {
+      deleteRecipientData(id);
+      handlePostDeleteModalOpen(false);
+      removeLocalStorageKey(id);
+      setTimeout(() => {
+        navigate(`/list`);
+      }, 500);
+    }
+  };
+
+  // localStorage에 저장된 key 값 제거함수
+  const removeLocalStorageKey = (id) => {
+    localStorage.removeItem(`${id}`);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleCheckClick();
+    }
   };
 
   return (
     <div className='post-delete-modal-wrapper' onClick={handleCancelClick}>
       <div
-        className='post-delete-modal-container'
+        className={
+          isFailConfirm ? 'post-delete-modal-container fail' : 'post-delete-modal-container'
+        }
         onClick={(e) => {
           e.stopPropagation();
         }}>
@@ -23,10 +79,25 @@ function PostDeleteModal({ handlePostDeleteModalOpen }) {
         <div className='post-delete-modal-container__pw-container'>
           <label htmlFor='pw'></label>
           <input
-            type='password'
+            type={isShowPw ? 'text' : 'password'}
             id='pw'
             className='post-delete-modal-container__pw-container__input'
+            ref={pwRef}
+            onKeyDown={handleKeyDown}
           />
+          {isShowPw ? (
+            <img
+              className='post-delete-modal-container__pw-container__pw-icon show'
+              src={showpwicon}
+              onClick={noneShowPwIconClick}
+            />
+          ) : (
+            <img
+              className='post-delete-modal-container__pw-container__pw-icon none-show'
+              src={noneshowpwicon}
+              onClick={showPwIconClick}
+            />
+          )}
         </div>
         <div className='post-delete-modal-container__btns'>
           <button id='post-delete-modal-container__btns-check-btn' onClick={handleCheckClick}>
