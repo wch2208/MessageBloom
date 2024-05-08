b588b36eb19a253540cd66a82cceb7560b387018
chanhee won <wch2208@gmail.com>
2024-05-08 15:48:13 +0900

    fix #69,#74: 입력폼 수정
    - enter 입력 방지
    - 에러 메세지 스타일 수정
    - formData 불러오는 함수명 변경
    - index.jsx로 page 컴포넌트 이름 변경 후 App.jsx 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import './App.scss';
 import Main from './pages/Main';
 import PostId from './pages/post-id';
 import List from './pages/list-page';
-import PostIdMessage from './pages/post-id-message-page/PostIdMessage';
-import Post from './pages/post-page/Post';
+import PostIdMessage from './pages/post-id-message-page';
+import Post from './pages/post-page';
 import Layout from './pages/layout/Layout';
 
 function App() {
