23a3571d1fde72600c24be2d57390ad6e61af1a1
chanhee won <wch2208@gmail.com>
2024-05-09 21:24:31 +0900

    merge: 불필요한 변경 제거

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/header/Header';
-import MouseTracker from '../components/commons/mouse-tracker';
 
 const Layout = () => {
   return (
     <>
-      <MouseTracker />
       <Header />
       <Outlet />
     </>
