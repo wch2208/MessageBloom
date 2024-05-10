079838129546e267b12e2d4d34d87594ccab7882
Hyunji <hjcho2303@naver.com>
2024-05-10 12:01:57 +0900

    Style: 이모지 이미지로 변경

--- src/pages/list-page/index.jsx
+++ src/pages/list-page/index.jsx
@@ -2,17 +2,25 @@ import './List.scss';
 import ListCardContainer from '../../components/list-page/list-card-container/index';
 import LinkButton from '../../components/commons/LinkButton';
 import { Link } from 'react-router-dom';
+import star_emoji from '../../assets/image/list/star_emoji.png';
+import fire_emoji from '../../assets/image/list/fire_emoji.png';
 
 export default function List() {
   return (
     <div className='list'>
       <div className='list__best'>
-        <p className='list__best-title'>인기 롤링 페이퍼 🔥</p>
+        <p className='list__best-title'>
+          인기 롤링 페이퍼
+          <img className='list__icon' src={fire_emoji} />
+        </p>
         <ListCardContainer sortLike='like' />
       </div>
 
       <div className='list__newest'>
-        <p className='list__newest-title'>최근에 만든 롤링 페이퍼 ⭐️</p>
+        <p className='list__newest-title'>
+          최근에 만든 롤링 페이퍼
+          <img className='list__icon' src={star_emoji} />
+        </p>
         <ListCardContainer sortLike='' />
       </div>
       <div className='list__create-btn__wrap'>
